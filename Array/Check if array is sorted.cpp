28900d52dd04e12d2065a42e555563ab63519526
MD AMANULLAH <92320933+amanullahmd12@users.noreply.github.com>
2023-02-23 23:08:53 +0530

    Create Check if array is sorted.cpp

---
+++ Array/Check if array is sorted.cpp
@@ -0,0 +1,16 @@
+//Check if array is sorted
+
+
+class Solution {
+  public:
+    bool arraySortedOrNot(int arr[], int n) {
+        // code here
+        
+        for(int i=1;i<n;i++){
+            if(arr[i] < arr[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
