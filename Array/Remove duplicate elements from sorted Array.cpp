6db1032c529a859968c48870388f8a3f499c9d1d
MD AMANULLAH <92320933+amanullahmd12@users.noreply.github.com>
2023-02-23 23:25:04 +0530

    Create Remove duplicate elements from sorted Array.cpp

---
+++ Array/Remove duplicate elements from sorted Array.cpp
@@ -0,0 +1,17 @@
+//Remove duplicate elements from sorted Array
+
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        // code here
+        
+        int i=0;
+        for(int j=1;j<n;j++){
+            if(a[i] != a[j]){
+                i++;
+                a[i]=a[j];
+            }
+        }
+        return i+1;
+    }
+};
